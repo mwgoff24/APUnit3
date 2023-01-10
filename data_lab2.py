66ccbcf4d3d80175e9e9ae01fed733afb8735836
Martin Goff <martingoff@Goffs-MacBook-Pro.local>
2023-01-10 08:49:59 -0600

    almost done, just one issue in displaying all max & min

--- data_lab2.py
+++ data_lab2.py
@@ -9,6 +9,29 @@ with open('BigData2016.csv', newline='') as csvfile:
         max_list = []
         min_list = []
         for row in reader:
-            if row['TMAX'] != "-996.00" and row[station]:
+            if row['TMAX'] != "-996.00" and row['STID'] == station:
                 max_list.append(float(row['TMAX']))
+            if row['TMIN'] != "-996.00" and row['STID'] == station:
+                min_list.append(float(row['TMAX']))
         print(max_list)
+        print(min_list)
+        # print(f"{station} high: {max(max_list)}")
+        # print(f"{station} low: {min(min_list)}")
+
+    define_max_and_min('ARD2')
+
+    define_max_and_min('BEAV')
+
+    define_max_and_min('BOIS')
+
+    define_max_and_min('CENT')
+
+    define_max_and_min('NRMN')
+
+    define_max_and_min('STIL')
+
+    define_max_and_min('TISH')
+
+    define_max_and_min('TULN')
+    
+    define_max_and_min('WOOD')
